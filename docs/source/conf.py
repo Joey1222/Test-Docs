e870df4f20461ca533505023b221c8d35475d10a
Joseph Knutson <101217373+Joey1222@users.noreply.github.com>
2023-08-09 13:56:42 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,8 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
+# -- Options for HTML outpu
+html_theme = ''
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
